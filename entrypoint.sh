03a750b8fd0f5c59b7841ce1d8df651f6a9e6025
SurceBeats <subcero@gmail.com>
2025-09-19 13:46:15 +0200

    2.6.12 > Basic SSL Support + Minor changes on SSL handling Entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-echo "Cleaning /app except atlas.ini and ssl..."
-find /app -mindepth 1 ! -name 'atlas.ini' ! -name 'ssl' -exec rm -rf {} +
+echo "Cleaning /app except atlas.ini and ./ssl if any..."
+find /app -mindepth 1 ! -name 'atlas.ini' ! -path '/app/ssl' ! -path '/app/ssl/*' -exec rm -rf {} +
 
 echo "Copying fresh contents from /app_defaults..."
 cp -r /app_defaults/* /app/
